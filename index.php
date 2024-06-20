01bf1ce38190f28d6f0fcdf675692eea1fb92b9f
alurosu <alurosu@gmail.com>
2024-06-20 10:55:16 +0300

    initial file

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php 
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en" data-bs-theme="dark">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Game - Among Demons</title>
+    <meta name="description" content="We converted the first demon models into Stargaze NFTs. Owning a piece makes you a supporter of the Among Demons project.">
+    <meta name="author" content="Among Demons">
+    <meta property="og:image" content="https://amongdemons.com/nfts/demons/faq/learnmore_founders_collection.png" />
+
+    <?php require_once("../data/parts/includes.php"); ?>
+  </head>
+  <body>
+    <?php require_once("../data/parts/nav.php"); ?>
+    <main class="container">
+      Hi
+    </main>
+    <?php require_once("../data/parts/footer.php"); ?>
+  </body>
+</html>
